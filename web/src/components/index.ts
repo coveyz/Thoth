b7a03a00d825702ea529c3d87b6559c77a53671e
coveyz <zhangkairong123@qq.com>
2026-03-18 21:20:53 +0800

    test: delte 删除文件 📃

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -2,8 +2,7 @@
  * @Author: coveyz zhangkairong123@qq.com
  * @Date: 2026-03-17 20:26:32
  * @LastEditors: coveyz zhangkairong123@qq.com
- * @LastEditTime: 2026-03-18 21:16:58
+ * @LastEditTime: 2026-03-18 21:20:40
  * @FilePath: /Thoth/web/src/components/index.ts
  */
 
-export { default as ChatView } from './chat/view.vue';
